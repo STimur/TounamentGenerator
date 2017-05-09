992369efef7243633ede2697e3c01a80d297062d
Timur <timur.salakhetdinov@gmail.com>
2017-05-09 03:34:23 +0300

    clean tests for Player

--- src/main/java/tournament_generator/Player.java
+++ src/main/java/tournament_generator/Player.java
@@ -8,6 +8,10 @@ public class Player {
     private String surname;
 
     public Player(String name, String surname) {
+        if (name.length() == 0)
+            throw new EmptyNameException();
+        if (surname.length() == 0)
+            throw new EmptySurnameException();
         this.name = name;
         this.surname = surname;
     }
@@ -19,4 +23,8 @@ public class Player {
     public String getSurname() {
         return surname;
     }
+
+    public class EmptyNameException extends RuntimeException {}
+
+    public class EmptySurnameException extends RuntimeException {}
 }
--- src/test/java/tournament_generator/PlayerTest.java
+++ src/test/java/tournament_generator/PlayerTest.java
@@ -1,5 +1,6 @@
 package tournament_generator;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -8,10 +9,26 @@ import static org.junit.Assert.*;
  * Created by Timur on 09-May-17.
  */
 public class PlayerTest {
-    @Test
-    public void playerMustHaveNameAndSurname() throws Exception {
-        Player player = new Player("Jack", "Sparrow");
+    private Player player;
+
+    private void assertPlayerHasNameAndSurname() {
         assertTrue(player.getName().length() > 1);
         assertTrue(player.getSurname().length() > 1);
+    }
+
+    @Test
+    public void playerMustHaveNameAndSurname() throws Exception {
+        player = new Player("Jack", "Sparrow");
+        assertPlayerHasNameAndSurname();
+    }
+
+    @Test(expected = Player.EmptyNameException.class)
+    public void whenEmptyName_ThenThrowsEmptyNameException() throws Exception {
+        player = new Player("", "Sparrow");
+    }
+
+    @Test(expected = Player.EmptySurnameException.class)
+    public void whenEmptyName_ThenThrowsEmptySurnameException() throws Exception {
+        player = new Player("Jack", "");
     }
 }
