854244b94d1047d9bd94c43e10767b9e97c762f4
Timur <timur.salakhetdinov@gmail.com>
2017-05-09 04:01:38 +0300

    clean tests for Administrator

--- src/main/java/tournament_generator/Administrator.java
+++ src/main/java/tournament_generator/Administrator.java
@@ -7,6 +7,7 @@ import java.util.List;
  * Created by Timur on 09-May-17.
  */
 public class Administrator {
+    public static final int MAX_PLAYERS_IN_TOURNAMENT = 16;
     private List<Player> registeredPlayers = new ArrayList();
 
     public int getNumberOfRegisteredPlayers() {
@@ -15,10 +16,13 @@ public class Administrator {
 
     public void registerPlayer(Player player) {
         if (registeredPlayers.size() == 16)
+            throw new NoSpaceRegistrationException();
+        if (player == null)
             throw new PlayerRegistrationException();
         registeredPlayers.add(player);
     }
 
-    public class PlayerRegistrationException extends RuntimeException {
-    }
+    public class NoSpaceRegistrationException extends RuntimeException {}
+
+    public class PlayerRegistrationException extends RuntimeException {}
 }
--- src/test/java/tournament_generator/AdministratorTest.java
+++ src/test/java/tournament_generator/AdministratorTest.java
@@ -1,6 +1,7 @@
 package tournament_generator;
 
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -11,33 +12,51 @@ import static org.junit.Assert.assertEquals;
 public class AdministratorTest {
 
     private Administrator admin;
+    private static Player[] players;
+
+    @BeforeClass
+    public static void createSeventeenPlayers() throws Exception {
+        players = new Player[17];
+        for (Integer i = 0; i < players.length; i++) {
+            players[i] = new Player(i.toString(), i.toString());
+        }
+    }
 
     @Before
     public void setUp() throws Exception {
         admin = new Administrator();
     }
 
+    private void registerSeventeenPlayers() {
+        for (Integer i = 0; i < players.length; i++) {
+            admin.registerPlayer(players[i]);
+        }
+    }
+
+    private void assertNumberOfRegisteredPlayersEquals(int expected) {
+        assertEquals(expected, admin.getNumberOfRegisteredPlayers());
+    }
+
     @Test
     public void whenNoPlayersRegistered_ThenNumberOfRegisteredPlayersEqualsZero() throws Exception {
-        assertEquals(0, new Administrator().getNumberOfRegisteredPlayers());
+        assertNumberOfRegisteredPlayersEquals(0);
+    }
+
+    @Test(expected = Administrator.PlayerRegistrationException.class)
+    public void givenNullPlayer_regiterPlayerThrowsPlayerRegistrationException() throws Exception {
+        admin.registerPlayer(null);
     }
 
     @Test
     public void whenNewPlayerRegistered_ThenNumberOfRegisteredPlayersIncrements() throws Exception {
-        admin.registerPlayer(new Player("Roger", "Federer"));
-        assertEquals(1, admin.getNumberOfRegisteredPlayers());
-        admin.registerPlayer(new Player("Raphael", "Nadal"));
-        assertEquals(2, admin.getNumberOfRegisteredPlayers());
+        for (int i = 0; i < Administrator.MAX_PLAYERS_IN_TOURNAMENT; i++) {
+            admin.registerPlayer(players[i]);
+            assertNumberOfRegisteredPlayersEquals(i+1);
+        }
     }
 
-    @Test(expected = Administrator.PlayerRegistrationException.class)
+    @Test(expected = Administrator.NoSpaceRegistrationException.class)
     public void WhenSeventeenthPlayerAttemptsToRegister_ThenThrowsPlayerRegistrationException() throws Exception {
         registerSeventeenPlayers();
-    }
-
-    private void registerSeventeenPlayers() {
-        Player players[] = new Player[17];
-        for (Player p : players)
-            admin.registerPlayer(p);
     }
 }
