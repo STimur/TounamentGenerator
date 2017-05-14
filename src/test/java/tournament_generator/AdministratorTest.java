ecc67bca8b42b7d805ecf6af4070d377bb7139b3
Timur <timur.salakhetdinov@gmail.com>
2017-05-14 16:38:54 +0300

    clean tests for Administrator class

--- src/test/java/tournament_generator/AdministratorTest.java
+++ src/test/java/tournament_generator/AdministratorTest.java
@@ -7,14 +7,12 @@ import org.junit.Test;
 
 import java.util.List;
 
-import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.*;
 
 /**
  * Created by Timur on 09-May-17.
  */
 public class AdministratorTest {
-
     private Administrator admin;
     private static Player[] players;
 
@@ -31,13 +29,6 @@ public class AdministratorTest {
         admin = new Administrator();
     }
 
-    private void registerSeventeenPlayers() {
-        final int PLAYERS_TO_REGISTER = 17;
-        for (Integer i = 0; i < PLAYERS_TO_REGISTER; i++) {
-            admin.registerPlayer(players[i]);
-        }
-    }
-
     private void assertNumberOfRegisteredPlayersEquals(int expected) {
         assertEquals(expected, admin.getNumberOfRegisteredPlayers());
     }
@@ -47,7 +38,6 @@ public class AdministratorTest {
             admin.registerPlayer(players[i]);
         }
     }
-
 
     private void assertPlayersAreAssignedToFirstRoundMatches() {
         for (Match m : admin.getFirstRoundMatches())
@@ -59,6 +49,69 @@ public class AdministratorTest {
         assertNotNull(m.getSecondPlayer());
     }
 
+    private void assertEachSeedTournamentChangesPlayersSeed() {
+        final int NUMBER_OF_SEEDS = 10;
+        List<Player> playersAfterSeed, playersAfterAnotherSeed;
+        for (int i = 0; i < NUMBER_OF_SEEDS; i++) {
+            admin.seedTournament();
+            playersAfterSeed = admin.getPlayersSeed();
+            admin.seedTournament();
+            playersAfterAnotherSeed = admin.getPlayersSeed();
+            assertNotEquals(playersAfterSeed, playersAfterAnotherSeed);
+        }
+    }
+
+    private void assertPlayersAreNotAssignedToFirstRoundMatches() {
+        for (Match m : admin.getFirstRoundMatches())
+            assertPlayersAreNotAssignedToMatch(m);
+    }
+
+    private void assertPlayersAreNotAssignedToMatch(Match m) {
+        assertNull(m.getFirstPlayer());
+        assertNull(m.getSecondPlayer());
+    }
+
+    private void assertThereAreFifteenMatchesInTournament() {
+        assertCertainSizeOfMatchesList(Administrator.NUMBER_OF_MATCHES_IN_TOURNAMENT, admin.getAllMatches());
+    }
+
+    private void assertThereAreTwoSemiFinalMatches() {
+        assertCertainSizeOfMatchesList(Administrator.NUMBER_OF_SEMIFINAL_MATCHES, admin.getSemiFinalMatches());
+    }
+
+    private void assertThereAreFourQuarterFinalMatches() {
+        assertCertainSizeOfMatchesList(Administrator.NUMBER_OF_QUARTER_FINAL_MATCHES, admin.getQuarterFinalMatches());
+    }
+
+    private void assertThereAreEightMatchesInFirstRound() {
+        assertCertainSizeOfMatchesList(Administrator.NUMBER_OF_FIRST_ROUND_MATCHES, admin.getFirstRoundMatches());
+    }
+
+    private void assertCertainSizeOfMatchesList(int size, List<Match> matches) {
+        assertEquals(size, matches.size());
+    }
+
+    private void assertFinalMatchIsLastInListOfAllMatches() {
+        assertEquals(admin.getAllMatches().get(Administrator.NUMBER_OF_MATCHES_IN_TOURNAMENT - 1), admin.getFinalMatch());
+    }
+
+    private void assertMatchHasNoNextMatch(Match finalMatch) {
+        assertNull(finalMatch.getNextMatch());
+    }
+
+    private void assertMatchesHaveCertainNextMatch(List<Match> matches, Match nextMatch) {
+        assertMatchHasNextMatch(matches.get(0), nextMatch);
+        assertMatchHasNextMatch(matches.get(1), nextMatch);
+    }
+
+    private void assertMatchHasNextMatch(Match match, Match nextMatch) {
+        assertEquals(nextMatch, match.getNextMatch());
+    }
+
+    private void assertMatchesHaveCertainNextRoundMatches(List<Match> matches, List<Match> nextMatches) {
+        for (int i = 0; i < nextMatches.size(); i++)
+            assertMatchesHaveCertainNextMatch(matches.subList(2 * i, 2 * i + 2), nextMatches.get(i));
+    }
 
     @Test
     public void whenNoPlayersRegistered_ThenNumberOfRegisteredPlayersEqualsZero() throws Exception {
@@ -74,47 +127,38 @@ public class AdministratorTest {
     public void whenNewPlayerRegistered_ThenNumberOfRegisteredPlayersIncrements() throws Exception {
         for (int i = 0; i < Administrator.MAX_PLAYERS_IN_TOURNAMENT; i++) {
             admin.registerPlayer(players[i]);
-            assertNumberOfRegisteredPlayersEquals(i+1);
+            assertNumberOfRegisteredPlayersEquals(i + 1);
         }
     }
 
     @Test(expected = Administrator.NoSpaceRegistrationException.class)
     public void WhenSeventeenthPlayerAttemptsToRegister_ThenThrowsPlayerRegistrationException() throws Exception {
-        registerSeventeenPlayers();
+        registerPlayers(Administrator.MAX_PLAYERS_IN_TOURNAMENT + 1);
     }
 
     @Test(expected = Administrator.SeedTournamentException.class)
-    public void WhenThereAreNoSixteenPlayersRegistered_SeedTournament_ThrowsTournamentSeedException() throws Exception {
+    public void givenThereAreNoSixteenPlayersRegistered_WhenSeedTournament_ThenThrowsTournamentSeedException() throws Exception {
         admin.seedTournament();
     }
 
     @Test
-    public void afterSeedTournamentGetSeededPlayersContainsSixteenPlayers() throws Exception {
+    public void givenSixteenPlayersAreRegistered_WhenSeedTournament_ThenPlayersSeedContainsSixteenPlayers() throws Exception {
         registerPlayers(Administrator.MAX_PLAYERS_IN_TOURNAMENT);
         admin.seedTournament();
-        assertTrue(admin.getPlayersSeed().size() == 16);
+        assertTrue(admin.getPlayersSeed().size() == admin.getNumberOfRegisteredPlayers());
     }
 
     @Test
     public void afterSeedTournamentPlayersAreNotInOrderOfRegistration() throws Exception {
         registerPlayers(Administrator.MAX_PLAYERS_IN_TOURNAMENT);
-        List<Player> playersBeforeSeed = admin.getRegisteredPlayers();
         admin.seedTournament();
-        List<Player> playersAfterSeed = admin.getPlayersSeed();
-        assertNotEquals(playersBeforeSeed, playersAfterSeed);
+        assertNotEquals(admin.getRegisteredPlayers(), admin.getPlayersSeed());
     }
 
     @Test
     public void afterEverySeedTournamentPlayersSeedIsDifferent() throws Exception {
         registerPlayers(Administrator.MAX_PLAYERS_IN_TOURNAMENT);
-        final int NUMBER_OF_SEEDS = 10;
-        for (int i = 0; i < NUMBER_OF_SEEDS; i++) {
-            admin.seedTournament();
-            List<Player> playersAfterSeed = admin.getPlayersSeed();
-            admin.seedTournament();
-            List<Player> playersAfterAnotherSeed = admin.getPlayersSeed();
-            assertNotEquals(playersAfterSeed, playersAfterAnotherSeed);
-        }
+        assertEachSeedTournamentChangesPlayersSeed();
     }
 
     @Test(expected = Administrator.PlayersNotSeededToStartTournamentException.class)
@@ -124,8 +168,7 @@ public class AdministratorTest {
 
     @Test
     public void givenPlayersAreNotSeeded_ThenPlayersAreNotAssignedToFirstRoundMatches() throws Exception {
-        assertNull(admin.getFirstRoundMatches().get(0).getFirstPlayer());
-        assertNull(admin.getFirstRoundMatches().get(0).getSecondPlayer());
+        assertPlayersAreNotAssignedToFirstRoundMatches();
     }
 
     @Test
@@ -137,66 +180,48 @@ public class AdministratorTest {
 
     @Test
     public void thereAreFifteenMatchesInTournament() throws Exception {
-        assertEquals(Administrator.NUMBER_OF_MATCHES_IN_TOURNAMENT, admin.getAllMatches().size());
+        assertThereAreFifteenMatchesInTournament();
     }
 
     @Test
     public void FinalMatchIsLastInListOfAllMatches() throws Exception {
-        assertEquals(admin.getAllMatches().get(Administrator.NUMBER_OF_MATCHES_IN_TOURNAMENT-1), admin.getFinalMatch());
+        assertFinalMatchIsLastInListOfAllMatches();
     }
 
     @Test
     public void thereAreTwoSemiFinalMatches() throws Exception {
-        assertEquals(Administrator.NUMBER_OF_SEMIFINAL_MATCHES, admin.getSemiFinalMatches().size());
+        assertThereAreTwoSemiFinalMatches();
     }
 
     @Test
     public void thereAreFourQuarterFinalMatches() throws Exception {
-        assertEquals(Administrator.NUMBER_OF_QUARTER_FINAL_MATCHES, admin.getQuarterFinalMatches().size());
+        assertThereAreFourQuarterFinalMatches();
     }
 
     @Test
     public void thereAreEightMatchesInFirstRound() throws Exception {
-        assertEquals(Administrator.NUMBER_OF_FIRST_ROUND_MATCHES, admin.getFirstRoundMatches().size());
+        assertThereAreEightMatchesInFirstRound();
     }
 
     @Test
     public void finalMatchHasNoNextMatch() throws Exception {
-        assertNull(admin.getFinalMatch().getNextMatch());
+        assertMatchHasNoNextMatch(admin.getFinalMatch());
     }
 
     @Test
     public void semiFinalMatchesHasFinalAsNextMatch() throws Exception {
-        assertEquals(admin.getFinalMatch(), admin.getSemiFinalMatches().get(0).getNextMatch());
-        assertEquals(admin.getFinalMatch(), admin.getSemiFinalMatches().get(1).getNextMatch());
+        assertMatchesHaveCertainNextMatch(admin.getSemiFinalMatches(), admin.getFinalMatch());
     }
 
     @Test
     public void quarterFinalMatchesHaveProperSemiFinalMatchAsNextMatch() throws Exception {
-        assertEquals(admin.getSemiFinalMatches().get(0), admin.getQuarterFinalMatches().get(0).getNextMatch());
-        assertEquals(admin.getSemiFinalMatches().get(0), admin.getQuarterFinalMatches().get(1).getNextMatch());
-
-        assertEquals(admin.getSemiFinalMatches().get(1), admin.getQuarterFinalMatches().get(2).getNextMatch());
-        assertEquals(admin.getSemiFinalMatches().get(1), admin.getQuarterFinalMatches().get(3).getNextMatch());
+        assertMatchesHaveCertainNextRoundMatches(admin.getQuarterFinalMatches(), admin.getSemiFinalMatches());
     }
 
     @Test
     public void firstRoundMatchesHaveProperQuarterFinalMatchAsNextMatch() throws Exception {
-        assertEquals(admin.getQuarterFinalMatches().get(0), admin.getFirstRoundMatches().get(0).getNextMatch());
-        assertEquals(admin.getQuarterFinalMatches().get(0), admin.getFirstRoundMatches().get(1).getNextMatch());
-
-        assertEquals(admin.getQuarterFinalMatches().get(1), admin.getFirstRoundMatches().get(2).getNextMatch());
-        assertEquals(admin.getQuarterFinalMatches().get(1), admin.getFirstRoundMatches().get(3).getNextMatch());
-
-        assertEquals(admin.getQuarterFinalMatches().get(2), admin.getFirstRoundMatches().get(4).getNextMatch());
-        assertEquals(admin.getQuarterFinalMatches().get(2), admin.getFirstRoundMatches().get(5).getNextMatch());
-
-        assertEquals(admin.getQuarterFinalMatches().get(3), admin.getFirstRoundMatches().get(6).getNextMatch());
-        assertEquals(admin.getQuarterFinalMatches().get(3), admin.getFirstRoundMatches().get(7).getNextMatch());
+        assertMatchesHaveCertainNextRoundMatches(admin.getFirstRoundMatches(), admin.getQuarterFinalMatches());
     }
-
-
-
 
     @Test
     public void whenTournamentIsNotFinished_ThenIsTournamentFinishedReturnsFalse() throws Exception {
